047b281cf858989f38053fb00912afdad855d9be
Corentin Cadiou <contact@cphyc.me>
2016-02-26 13:42:38 +0100

    add argument supports
    * possible to pick a custom application to open the link with
    * possible to print the link onto stdout

--- torrent.js
+++ torrent.js
@@ -1,33 +1,40 @@
 #!/usr/bin/env node
 var choices = require('choices'),
-	search = require('search-kat.ph'),
-	open = require('open'),
-	query = process.argv[2],
-	path = require('path');
+    search = require('search-kat.ph'),
+    open = require('open'),
+    path = require('path'),
+    cmdLineArgs = require('command-line-args');
 
-var showUsage = function showUsage() {
-	var pathToBin = path.join(
-		path.relative(
-			process.cwd(),
-			path.dirname(process.argv[1])
-		),
-		path.basename(process.argv[1])
-	);
+var cli = cmdLineArgs([
+    {
+	name: 'application', alias: 'a', type: String,
+	description: 'Application to use to open link. By default use system\'s default application'},{
+	name: 'stdout', alias: 's', type: Boolean, defaultValue: false,
+	description: 'Output the link to stdout instead of sending it to default application'
+    },{
+	name: 'query', alias: 'q', type: String, defaultOption: true,
+	description: 'The query to search the link'
+    },{
+	name: 'help', alias: 'h', type: Boolean, defaultValue: false,
+	description: 'Print this help'
+    }
+]);
 
-	console.log('Usage:');
-	console.log(process.argv[0] + ' ' + pathToBin + ' "query"');
-};
+var options = cli.parse();
 
-if (!query) {
-	showUsage();
-	process.exit();
+if (!options.query || options.help) {
+    console.log(cli.getUsage());
+    process.exit();
 }
 
-search(query).then(function(results) {
-		choices('Select a torrent', results.slice(0, 9).map(function(r) { return r.name + ' [' + r.size + ' / ' + r.files + ' files] ' + r.seeds + '/' + r.leech; }), function(idx) {
-			if (idx === null) {
-				return;
-			}
-			open(results[idx].magnet);
-		});
+search(options.query).then(function(results) {
+    choices('Select a torrent', results.slice(0, 9).map(function(r) { return r.name + ' [' + r.size + ' / ' + r.files + ' files] ' + r.seeds + '/' + r.leech; }), function(idx) {
+	if (idx === null) {
+	    return;
+	}
+	if (!options.stdout)
+       	    open(results[idx].magnet, options.application);
+	else
+	    console.log(results[idx].magnet)
+    });
 });
